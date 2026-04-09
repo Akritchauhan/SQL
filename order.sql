b5e293fe5952522e2163606deaad8312b1410b7e
Akrit Chauhan <156348636+Akritchauhan@users.noreply.github.com>
2026-04-09 16:20:09 +0530

    learned about clause order

---
+++ order.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE payment;
+USE payment;
+
+CREATE TABLE method(
+customer_id INT PRIMARY KEY,
+customer VARCHAR(50),
+mode VARCHAR(50),
+city VARCHAR(50)
+);
+
+INSERT INTO method 
+(customer_id,customer,mode,city)
+VALUES
+(101,"OLIVIA","netbanking","poland"),
+(102,"ETHAN","netbanking","Finland"),
+(103,"ROHAN","Credit CARD","India"),
+(104,"SOPHIA","Credit CARD","Uk"), 
+(105,"JACKSON","netbanking","USA");
+
+SELECT city 
+FROM method 
+WHERE mode ="netbanking"
+GROUP BY mode
+HAVING customer_id > 101;
